e2ad111bf1b3ff91a57d1ad2702e222cbb4b5527
Ethan Cao <wkt7ne@virginia.edu>
2024-11-23 19:13:46 -0500

    Revert "lab8 code snippets"
    This reverts commit e88a347d2f4d93d48baea5303f10411fc2d4fe0f.

--- lab8/fetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-URL="https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85"
-
-curl -s "$URL" | jq -r '.[] | .receiptTime' | head -n 6
--- list-queues.py
+++
@@ -1,7 +0,0 @@
-import boto3
-import json 
-from credentials import *
-
-response = sqs.list_queues()
-
-print(response)

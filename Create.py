e88a347d2f4d93d48baea5303f10411fc2d4fe0f
Ethan Cao <wkt7ne@virginia.edu>
2024-11-23 19:07:18 -0500

    lab8 code snippets

---
+++ Create.py
@@ -0,0 +1,20 @@
+import json
+
+myvals = [{"repo_name":"json-practice", "repo_url":"https://github.com/nmagee/json-practice/"}, {"repo_name":"fastapi-demo", "repo_url":"https://github.com/nmagee/fastapi-demo/"}]
+myjson = json.dumps(myvals)
+
+first = ('sape', 4139)
+second = ('guido', 4127)
+third = ('jack', 4098)
+
+# combine tuples into list. Can .append one by one or .extend all at once
+mylist = []
+mylist.extend((first, second, third))
+
+# convert to dict
+mydict = dict(mylist)
+mydict
+
+# convert to JSON
+myjson = json.dumps(mydict)
+myjson
